0a2eb0c391966365627d594f56c6b2c42e03f590
EdoardoFerrini <edoardo.ferrini@gmail.com>
2024-06-22 16:56:55 +0200

    aggiunto AccountService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,38 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from './_services/account.service';
+import { User } from './_models/user'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent implements OnInit {
   title: string = 'angular_Restore';
   users: any;
-  constructor(private http: HttpClient) {}
+
+  constructor(private http: HttpClient, private accountService : AccountService) {}
+
   ngOnInit(): void {
+    this.getUsers();
+    this.setCurrentUser();
+  }
+
+  getUsers(){
     this.http.get('https://localhost:7174/api/User').subscribe({
       next : response => this.users = response,
       error : error => console.log(error),
       complete : () => console.log("Request has been completed.")
     });
   }
+
+  setCurrentUser(){
+    const userString = localStorage.getItem('user');
+    if (!userString) return;
+    const user: User = JSON.parse(userString);
+    this.accountService.setCurrentUser(user);
+  }
+
 }
--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../_services/account.service';
 
 @Component({
   selector: 'app-nav',
@@ -6,14 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './nav.component.css',
 })
 export class NavComponent implements OnInit {
-  model: any = {}
-  
-  constructor() {}
-  
-  ngOnInit(): void {
+  model: any = {};
+  loggedIn = false;
+  constructor(private accountService: AccountService) {}
+
+  ngOnInit(): void {}
+
+  getCurrentUser() {
+    this.accountService.currentUsers$.subscribe({
+      next: (user) => (this.loggedIn = !!user),
+      error: (error) => console.log(error),
+    });
   }
 
   login() {
-    console.log(this.model)
+    console.log('this.model', this.model);
+    this.accountService.login(this.model).subscribe({
+      next: (response) => {
+        console.log(response);
+        this.loggedIn = true;
+      },
+      error: (error) => console.log(error),
+    });
+  }
+
+  logout() {
+    this.accountService.logout();
+    this.loggedIn = false;
   }
 }
